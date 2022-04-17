4ab63372ef9cba166fe5d928eefff5e25a0a091c
Amankaium <kaium@list.ru>
2022-04-17 21:16:28 +0600

    add test

--- shop/mainapp/tests.py
+++ shop/mainapp/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from requests import get
 
 # Create your tests here.
+
+class HomepageTest(TestCase):
+    def test_homepage_200(self):
+        response = get("http://127.0.0.1:8000/")
+        assert response.status_code == 200
