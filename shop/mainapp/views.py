146c2578388f4451e3a766872c891f75350c2a26
irshatbek <irshatbek@gmail.com>
2022-04-21 06:14:37 +0600

    added registration and login items to navbar and rename logup into register

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'mainapp/home.html')
 
-def signup(request):
-    return render(request, 'mainapp/signup.html')
+def register(request):
+    return render(request, 'mainapp/register.html')
 
 def login(request):
     return render(request, 'mainapp/login.html')
