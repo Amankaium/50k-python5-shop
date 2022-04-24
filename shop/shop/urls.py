ab70f72bacd6d7d71dbdc714cc339a9f9fe2571e
irshatbek <irshatbek@gmail.com>
2022-04-24 21:05:08 +0600

    added new logout

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('mainapp.urls')),
     path('accounts/', include('accounts.urls')),
     path('socialaccounts/', include('allauth.urls')),
+
 ] 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
