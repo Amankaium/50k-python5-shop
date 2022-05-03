4995acd9c995d1192a554c6969b078993f6fe11f
irshatbek <35058278+irshatbek@users.noreply.github.com>
2022-05-03 21:43:25 +0600

    Update urls.py

--- shop/mainapp/urls.py
+++ shop/mainapp/urls.py
@@ -2,8 +2,5 @@ from django.urls import path, include
 from . import views
 urlpatterns = [
     path('', views.home, name='home' ),
-    path('product/', views.product, name='product'),
-
-    
 
 ]
