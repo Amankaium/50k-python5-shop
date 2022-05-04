38adce526f5190992f0eb15a0c9a176f75f00a0d
madina myrzakulova <madina.japarbekovna@jmail.com>
2022-05-04 19:30:55 +0600

    authorization testing

--- shop/mainapp/tests.py
+++ shop/mainapp/tests.py
@@ -1,27 +1,42 @@
-from urllib import response
+
 from django.test import TestCase
 from django.urls import reverse
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from requests import get
+from time import sleep
+from selenium.webdriver.common.by import By
 
 
 
-class HomepageTest(TestCase):
-    def test_homepage_200(self):
-        response = get("http://127.0.0.1:8000/")
-        assert response.status_code == 200
+
+# class HomepageTest(TestCase):
+#     def test_homepage_200(self):
+#         response = get("http://127.0.0.1:8000/")
+#         assert response.status_code == 200
+ 
+
+class SeleniumTest(TestCase):
+    def test_activation(self):
+        driver = webdriver.Chrome()
+        driver.get("http://localhost:8000")
+        driver.find_element_by_link_text('Войти').click()
+        
+        name = driver.find_element_by_name('username')
+        name.send_keys('Тестовая введения имя админа')
+
+        password = driver.find_element_by_name('password')
+        password.send_keys('Тестовая введения пароля')
+
+        button= driver.find_element_by_xpath("//*[contains(., 'Войти')]")
+        
+        button.click()
+      
+        sleep(5)
+        driver.close()
+      
 
 
-
-class loginTest(TestCase):
-    def test_login(self):
-        url = reverse('login')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code,200) 
-
-
-     
 
 
--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -89,17 +89,14 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'shop_db',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
