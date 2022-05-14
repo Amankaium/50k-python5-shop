358fe27ee6c5dd051ce315b92218b249f39d876b
EduardSavlukov <eduardsavlu@gmail.com>
2022-05-15 01:50:47 +0600

    - Create model Cart     - Create model Category     - Create model Smartphone     - Create model Notebook     - Create model Customer     - Create model CartProduct     - Add field owner to cart     - Add field products to cart

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -1,1 +1,143 @@
+# from unicodedata import category
+# from certifi import contents
+from django.db import models
+# from numpy import product
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
+User = get_user_model()
+
+# Create your models here.
+
+# Созд моделей
+# 1 - Category
+# 2 - Product
+# 3 - CartProduct
+# 4 - Cart
+# 5 - Order
+
+# 6 - Customer
+# 7 - Specification
+
+class Category(models.Model):
+    
+    name = models.CharField(max_length=255, verbose_name='Имя категории')
+    slug = models.SlugField(unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    
+    class Meta:
+        abstract = True
+    
+    category = models.ForeignKey(Category, verbose_name='Каеугория', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, verbose_name='Наименование')
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(verbose_name='Изображение')
+    discripnion = models.TextField(verbose_name='Описание', null=True)
+    prise = models.DecimalField(max_digits=9, decimal_places=2,verbose_name='Цена')
+    
+    def __str__(self):
+        return self.title
+    
+# **************************************
+# Ноутбуки
+# Dioganal
+# Display
+# Processor
+# Ram
+# Video
+# time_without_charge
+
+
+class Notebook(Product):
+    
+    dioganal = models.CharField(max_length=255, verbose_name='Диагональ')
+    display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
+    processor = models.CharField(max_length=255, verbose_name='Частота процессора')
+    ram = models.CharField(max_length=255, verbose_name='Оперативная память')
+    video = models.CharField(max_length=255, verbose_name='Видеокарта')
+    time_without_charge = models.CharField(max_length=255, verbose_name='Время работы батареии')
+    
+    def __str__(self):
+        return "{} : {}".format(self.category.name, self.title)
+    
+# **************************************
+
+# Смартфоны
+# Dioganal
+# Display
+# Resolution
+# Acumm_volume
+# Ram
+# sd
+# sd_volume
+# main_cam_mp
+# frontal_cam_mp
+
+class Smartphone(Product):
+    
+    dioganal = models.CharField(max_length=255, verbose_name='Диагональ')
+    display_type = models.CharField(max_length=255, verbose_name='Тип дисплея')
+    resolotion =models.CharField(max_length=255, verbose_name='Разрешение экрана')
+    acum_volume = models.CharField(max_length=255,verbose_name='Объем батареи')
+    ram = models.CharField(max_length=255, verbose_name='Оперативная память')
+    sd = models.BooleanField(default=True)
+    sd_volume = models.CharField(max_length=255, verbose_name='Объем встроенной памяти')
+    main_cam_mp = models.CharField(max_length=255, verbose_name='Главная камера')
+    frontal_cam_mp = models.CharField(max_length=255, verbose_name='Фронтальная камера')
+    
+    def __str__(self):
+        return "{} : {}".format(self.category.name, self.title)
+# *******************************************************************************************
+
+    
+class CartProduct(models.Model):
+    
+    user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_products')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    # product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE)
+    qty = models.PositiveIntegerField(default=1)
+    final_prace = models.DecimalField(max_digits=9, decimal_places=2,verbose_name='Общая стоимость')
+    
+    def __str__(self):
+        return "Продукт: {} (для корзины)".format(self.product.title)
+    
+
+class Cart(models.Model):
+    
+    owner = models.ForeignKey('Customer', verbose_name='Владелец', on_delete=models.CASCADE)
+    products = models.ManyToManyField(CartProduct, blank=True, related_name='related_cart')
+    total_products = models.PositiveIntegerField(default=0)
+    final_prace = models.DecimalField(max_digits=9, decimal_places=2,verbose_name='Общая стоимость')
+    
+    def __str__(self):
+        return str(self.id)
+    
+    
+class Customer(models.Model):
+    
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    phone = models.CharField(max_length=20, verbose_name='Номер телефона')
+    adress = models.CharField(max_length=255, verbose_name='Адрес')
+    
+    def __str__(self):
+        return "Покупатель: {} {}".format(self.user.first_name, self.user.last_name)
+    
+    
+# class Specification(models.Model):
+    
+#     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+#     object_id = models.PositiveIntegerField()
+#     name = models.CharField(max_length=255, verbose_name='Наименование товара для характеристик')
+    
+#     def __str__(self) -> str:
+#         return "Характеристики для товара: {}".format(self.name)
+    
