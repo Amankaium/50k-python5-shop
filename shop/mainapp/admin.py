717fcfe767f0b7cf14eebde08eb59a869723800f
EduardSavlukov <eduardsavlu@gmail.com>
2022-05-18 01:25:15 +0600

    create folders for img

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
 from .models import *
 
+
+'''
+from django import forms
+class NotebookCategoryChoiceField(forms.ModelChoiceField):
+    
+    pass
+    
+    
+class NotebookAdmin(admin.ModelAdmin):
+    
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return NotebookCategoryChoiceField(Category.objects.filter(slug='notebooks'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+    
+    
+class SmatrphoneCategoryChoiceField(forms.ModelChoiceField):
+    
+    pass
+    
+    
+class SmartphoneAdmin(admin.ModelAdmin):
+    
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            return NotebookCategoryChoiceField(Category.objects.filter(slug='smartphones'))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+'''
 # Register your models here.
 admin.site.register(Category)
 # admin.site.register(Notebook)
