8d3a2784f8a37f8c22adb273d4889b5db880ac14
madina myrzakulova <madina.japarbekovna@jmail.com>
2022-06-07 23:51:03 +0600

    added html for orders

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('', include('mainapp.urls')),
     path('accounts/', include('accounts.urls')),
     path('socialaccounts/', include('allauth.urls')),
