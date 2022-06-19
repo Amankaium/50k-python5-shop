1b689f2f9f64962d6c95aa899d3636a584c1c258
madina myrzakulova <madina.japarbekovna@jmail.com>
2022-06-19 12:32:55 +0600

    added transition to product page

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import *
 
-# Create your views here.
-def home(request):
-    product_objects = Product.objects.all()
-    return render(request, 'mainapp/home.html',{'products': product_objects})
+
+
+
+
+def home(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+    return render(request,
+                  'mainapp/home.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products})
+
 
 def product(request):
-    return render(request, 'mainapp/product.html')
+    product = Product.objects.all()
+    parameters = Parameter.objects.all()             
+    return render(request,
+                  'mainapp/product.html',
+                  {'products': product,
+                  'parameters': parameters})
 
+
+                  
